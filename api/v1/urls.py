39476b91ecfd342cb842e3c835ee1305620de4a6
hridesh-net <hrideshkhandal@outlook.com>
2024-03-27 17:21:23 +0530

    Urls Updated

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from django.urls import include
 
 from applicants.views import ApplicantView as Applicant
+from marketing.views import OrganisationView as Org, JobView as Job
 
 
 urlpatterns = [
-    path("applicant/create/", Applicant.as_view(), name="create-applicant"),
+    path("applicant/", Applicant.as_view(), name="create-applicant"),
+    path("org/create/", Org.as_view(), name="create-organisation"),
+    path("job/", Job.as_view(), name="create-job"),
 ]
