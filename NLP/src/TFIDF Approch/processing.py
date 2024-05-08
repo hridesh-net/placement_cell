99553f042320692800688d85f0bfc6472d1a205c
Dhruv-net <soni10682@gmail.com>
2024-05-09 00:41:48 +0530

    Made api for nlp

--- NLP/src/TFIDF Approch/processing.py
+++ NLP/src/TFIDF Approch/processing.py
@@ -102,3 +102,14 @@ print('\n')
 print(train_lemma[0])
 print('===========')
 print(test_lemma[0])
+
+
+
+prompt = """Analyze the file and create me a table of persons who are late like 
+    
+    person name:
+    person emp code:
+    late more then 15mins: how many time that also 
+    late more then 30 mins : how many time that also 
+    
+    """
