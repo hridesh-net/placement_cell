14e88964c7325a3350e19f40b274f5637a0d47e0
kanishk <kanishktiwari11a@gmail.com>
2024-07-25 20:22:32 +0530

    feature/trial

--- resume_analyzer/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'resume_analyzer.apps.ResumeAnalyzerConfig'
--- resume_analyzer/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class FileUploadSerializer(serializers.Serializer):
-    resume = serializers.FileField()
-    job_description = serializers.FileField()
